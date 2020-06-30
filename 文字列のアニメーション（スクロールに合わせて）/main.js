01003d7bbe7b010aac2c0a79834cd30b3f6d982b
Koudai <2110koudai@gmail.com>
2020-06-30 22:43:24 +0900

    新しくクラスを作成して使いまわしやすいように変更

--- 文字列のアニメーション（スクロールに合わせて）/main.js
+++ 文字列のアニメーション（スクロールに合わせて）/main.js
@@ -1,21 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   //domの要素がロードされたときに実行される
-  const els = document.querySelectorAll(".animate-title"); //.animate-titleの要素を全て取得する。配列でelsに格納される
-  const cb = function (entries, observer) {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const ta = new TextAnimation(entry.target); //object作成
-        ta.animate();
-        observer.unobserve(entry.target);
-      } else {
-      }
-    });
+
+  const cb = function (el, isIntersecting) {
+    if (isIntersecting) {
+      const ta = new TextAnimation(el);
+      ta.animate();
+    }
   };
-  const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0,
-  };
-  const io = new IntersectionObserver(cb, options);
-  els.forEach((el) => io.observe(el));
+
+  const so = new ScrollObserver(".animate-title", cb);
 });
