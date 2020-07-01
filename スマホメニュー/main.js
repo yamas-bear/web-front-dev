356606a17b620a72653c94c00c598c4ee0489f2b
Koudai <2110koudai@gmail.com>
2020-07-01 15:02:44 +0900

    デザインも含め完成

--- スマホメニュー/main.js
+++ スマホメニュー/main.js
@@ -3,6 +3,7 @@ class MobileMenu {
     this.DOM = {};
     this.DOM.btn = document.querySelector(".mobile-menu__btn");
     this.DOM.container = document.querySelector("#global-container");
+    this.DOM.cover = document.querySelector(".mobile-menu__cover");
     this.eventType = this._getEventType();
     this._addEvent();
   }
@@ -14,6 +15,7 @@ class MobileMenu {
   }
   _addEvent() {
     this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+    this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
   }
 }
 
